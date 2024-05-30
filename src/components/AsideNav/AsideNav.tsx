6d0b67a55a2ae4e75df48dde2015d0ba0b9979e8
Serhii Shabala <sergei181989@gmail.com>
2024-05-30 13:53:09 +0300

    :recycle: Refactor Aside component

--- src/components/AsideNav/AsideNav.tsx
+++ src/components/AsideNav/AsideNav.tsx
@@ -1,10 +1,25 @@
+import { routers } from '@/lib/constsnts';
+import { CiLogout } from 'react-icons/ci';
+import { AsideNavLink } from '../AsideNavLink';
+import { Icon } from '../ui';
+
 export const AsideNav: React.FC = () => {
   return (
     <nav className='flex flex-col h-full'>
-      <ul className='flex flex-col flex-1 mb-2'>
-        <li className='p-4 rounded-xl bg-white text-rose-500'>Dashboard</li>
+      <ul className='flex flex-col flex-1 gap-y-2 mb-2'>
+        {routers.map((router) => (
+          <AsideNavLink
+            key={router.id}
+            router={router}
+          />
+        ))}
       </ul>
-      <button className='flex flex-shrink-0 p-4 rounded-xl bg-white text-rose-500'>
+      <button className='flex flex-shrink-0 items-center p-4 rounded-xl transition-all text-white hover:bg-white hover:text-rose-500'>
+        <Icon
+          name={CiLogout}
+          size={24}
+          className='mr-5'
+        />
         Logout
       </button>
     </nav>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,13 @@
+import { IconType } from 'react-icons';
+
 export interface RouteError {
   statusText?: string;
   message?: string;
+}
+
+export interface NavLinks {
+  id: number;
+  to: string;
+  title: string;
+  iconName: IconType;
 }
