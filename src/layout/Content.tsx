f6b6d859ae4ab8f5c25349a22564ccda6383026f
Serhii Shabala <sergei181989@gmail.com>
2024-05-31 15:44:00 +0300

    :recycle: Refactor routing

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -1,10 +1,10 @@
-import { Dashboard } from '@/components';
 import { Box } from '@/components/ui';
+import { Outlet } from 'react-router-dom';
 
 export const Content: React.FC = () => {
   return (
     <Box className='w-full max-w-[960px]'>
-      <Dashboard />
+      <Outlet />
     </Box>
   );
 };
--- src/routes/roots.tsx
+++ src/routes/roots.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from '@/components';
 import { Home, NotFound } from '@/pages';
 import { createBrowserRouter } from 'react-router-dom';
 
@@ -6,5 +7,31 @@ export const router = createBrowserRouter([
     path: '/',
     element: <Home />,
     errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: 'vital-task',
+        element: <div>Vital Task</div>,
+      },
+      {
+        path: 'my-task',
+        element: <div>My Task</div>,
+      },
+      {
+        path: 'category',
+        element: <div>Category Task</div>,
+      },
+      {
+        path: 'settings',
+        element: <div>Settings</div>,
+      },
+      {
+        path: 'help',
+        element: <div>Help</div>,
+      },
+    ],
   },
 ]);
