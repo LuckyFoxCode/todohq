14e1a2b9cfad9e9a8edd180e76d3818b3163051f
Serhii Shabala <sergei181989@gmail.com>
2024-06-08 15:20:31 +0300

    :recycle: Refactor: add other routers.

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -7,6 +7,26 @@ export const router = createBrowserRouter([
     element: <Home />,
   },
   {
+    path: '/vital-task',
+    element: <div>Vital Task</div>,
+  },
+  {
+    path: '/my-task',
+    element: <div>My Task</div>,
+  },
+  {
+    path: '/task-categories',
+    element: <div>Task Categories</div>,
+  },
+  {
+    path: '/settings',
+    element: <div>Settings</div>,
+  },
+  {
+    path: '/help',
+    element: <div>Help</div>,
+  },
+  {
     path: '*',
     element: <div>Not Found</div>,
   },
